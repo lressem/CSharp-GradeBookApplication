84d82fe597ea59648f82951b5bed7d60bbc647fe
s236409@stud.hioa.no <lev.ressem@avanade.com>
2018-08-08 22:21:53 +0200

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -12,6 +13,49 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            //how many students to drop to next 20%
+            var dropIndexOfGrade = (int)Math.Ceiling(Students.Count * 0.2);
+
+            
+            //creates a sorted list of grades in descending order using linq
+            var sortedListOfGrades = Students.OrderByDescending(st => st.AverageGrade).Select(st => st.AverageGrade).ToList();
+
+            char grade;
+
+            if (sortedListOfGrades[dropIndexOfGrade - 1] <= averageGrade)
+            {
+                grade = 'A';
+                return grade;
+            }
+
+            else if (sortedListOfGrades[dropIndexOfGrade * 2 - 1] <= averageGrade)
+            {
+                grade = 'B';
+                return grade;
+            }
+
+            else if (sortedListOfGrades[dropIndexOfGrade * 3 - 1] <= averageGrade)
+            {
+                grade = 'C';
+                return grade;
+            }
+
+            else if (sortedListOfGrades[dropIndexOfGrade * 4 - 1] <= averageGrade)
+            {
+                grade = 'D';
+                return grade;
+            }
+
+            else return 'F';
+
+        }
 
     }
 }
